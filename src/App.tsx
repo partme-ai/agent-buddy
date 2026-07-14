1f9ecc755f7bf3a58f68a88790e451cce2ead6a0
Loong Wan <teachingai@163.com>
2026-07-14 20:17:11 +0800

    feat: mount instance governance dock

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import ConsoleAppComplete from './ConsoleAppComplete'
 import InspectorDrawer from './InspectorDrawer'
+import InstanceGovernanceDock from './InstanceGovernanceDock'
 import OperationalControls from './OperationalControls'
 
 export default function App() {
   return <>
     <ConsoleAppComplete />
     <OperationalControls />
+    <InstanceGovernanceDock />
     <InspectorDrawer />
   </>
 }
