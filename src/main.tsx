453da5c7a999d1fbadf24f0f9631b5dbb541a7bc
Loong Wan <teachingai@163.com>
2026-07-14 22:20:58 +0800

    Import protocol registration styles

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import './styles.css'
 import './layout.css'
 import './complete-console.css'
 import './instance-governance.css'
+import './protocol-registration.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
