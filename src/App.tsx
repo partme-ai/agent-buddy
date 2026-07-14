6196ad4024872fe3457548e6d221049139621ff2
Loong Wan <teachingai@163.com>
2026-07-14 15:53:02 +0800

    feat: mount persistent inspector drawer

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import ConsoleAppComplete from './ConsoleAppComplete'
+import InspectorDrawer from './InspectorDrawer'
 import OperationalControls from './OperationalControls'
 
 export default function App() {
   return <>
     <ConsoleAppComplete />
     <OperationalControls />
+    <InspectorDrawer />
   </>
 }
