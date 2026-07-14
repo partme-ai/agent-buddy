cc585c4679af4f1d29ea42e9e3ae260fe3a54055
Loong Wan <teachingai@163.com>
2026-07-14 22:20:30 +0800

    Mount protocol registration dock

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import ConsoleAppComplete from './ConsoleAppComplete'
 import InspectorDrawer from './InspectorDrawer'
 import InstanceGovernanceDock from './InstanceGovernanceDock'
 import OperationalControls from './OperationalControls'
+import ProtocolRegistrationDock from './ProtocolRegistrationDock'
 
 export default function App() {
   return <>
     <ConsoleAppComplete />
     <OperationalControls />
     <InstanceGovernanceDock />
+    <ProtocolRegistrationDock />
     <InspectorDrawer />
   </>
 }
